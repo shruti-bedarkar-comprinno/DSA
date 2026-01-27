f3a8b60561c25908ea7379aba932e94d41df28c2
Shruti Bedarkar <shruti@example.com>
2026-01-28 03:40:25 +0530

    Added sorting and string programs

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Bubble Sort: ");
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
