d19e4ef01a8c8b8c3550885771ee267654909e73
shruti-bedarkar-comprinno <shruti.bedarkar@comprinno.net>
2026-02-25 23:55:04 +0530

    Delete String/WordCount.java

--- String/WordCount.java
+++
@@ -1,8 +0,0 @@
-public class WordCount {
-    public static void main(String[] args) {
-        String str = "Java is very easy";
-        String[] words = str.split(" ");
-
-        System.out.println("Word count: " + words.length);
-    }
-}
