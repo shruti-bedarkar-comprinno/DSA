c6fb268cf38a2bda55d63a464201ad398fa92836
shruti-bedarkar-comprinno <shruti.bedarkar@comprinno.net>
2026-02-25 13:00:10 +0530

    Update ReverseQueue.java

--- Queue/ReverseQueue.java
+++ Queue/ReverseQueue.java
@@ -3,30 +3,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String args[]) {
-
         Queue<Integer> q = new LinkedList<>();
-
         q.add(1);
         q.add(2);
         q.add(3);
         q.add(4);
-
         Stack<Integer> s = new Stack<>();
-
-       
         while(!q.isEmpty()) {
             s.push(q.remove());
         }
-
-       
         while(!s.isEmpty()) {
             q.add(s.pop());
         }
-
-       
         while(!q.isEmpty()) {
             System.out.print(q.remove() + " ");
         }
-
     }
 }
