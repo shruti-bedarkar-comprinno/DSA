7afda1ba2a8fe51d7872b037f9318433b65f6503
shruti-bedarkar-comprinno <shruti.bedarkar@comprinno.net>
2026-02-25 21:40:08 +0530

    Update reval.java

--- Array/reval.java
+++ Array/reval.java
@@ -1,38 +1,28 @@
 import java.util.ArrayList;
+
 class reval {
-public static void main(String[] args) {
-    public static void sawp(ArrayList<Integer> list , int ind1, int ind2 ) {
-    int temp = list.get(ind1);
-    list.set(ind1, list.get(ind2));
-    list.set(ind2, list.get(temp));
 
-}
-System.out.println(list);
+    
+    public static void swap(ArrayList<Integer> list, int ind1, int ind2) {
+        int temp = list.get(ind1);
+        list.set(ind1, list.get(ind2));
+        list.set(ind2, temp);
+    }
 
-}
-    ArrayList<Integer> list = new ArrayList<>();
-     list.add(12);
-     list.add(34);
-     list.add(45);
-     list.add(10000);
-    //  for(int i=0;i<a.size();i++){
-    //  System.out.println(a.get(i));
-    //  }
-    //  System.out.println("--------");
-    //  for(int i=a.size()-1;i>=0;i--) {
-    //     System.out.println(a.get(i));
-    //  }
-   //     int max = Integer.MIN_VALUE;
-  //     for(int i=0;i<a.size();i++) 
-   //     {
-   //         if(a.get(i)>max){
-  //             max = a.get(i);
-//         }
-//     }
-// System.out.println("maximum value will be:" + max);
-//swap code
-}
-}
+    public static void main(String[] args) {
 
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(34);
+        list.add(45);
+        list.add(10000);
+
+        System.out.println("Before swap: " + list);
+
+        swap(list, 1, 2);  // swap index 1 and 2
+
+        System.out.println("After swap: " + list);
+    }
+}
 
 
