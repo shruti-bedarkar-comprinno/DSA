5b014b43371f98bd1ceae89bd2fdfde08087da50
shruti-bedarkar-comprinno <shruti.bedarkar@comprinno.net>
2026-02-25 23:20:34 +0530

    Update EvenDigits.java

--- Searching/EvenDigits.java
+++ Searching/EvenDigits.java
@@ -1,11 +1,40 @@
 public class EvenDigits {
-    public static void EVenDigits(int arr[]) {
-        int count =0;
-        for(int i=0;i<arr.length;i++){
-            
+
+    public static int evenDigits(int arr[]) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int digits = countDigits(arr[i]);
+
+            if (digits % 2 == 0) {
+                count++;
+            }
         }
+
+        return count;
     }
+
+   
+    public static int countDigits(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        int count = 0;
+
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
-        
+        int arr[] = {12, 345, 2, 6, 7896};
+
+        int result = evenDigits(arr);
+
+        System.out.println("Numbers with even digits: " + result);
     }
 }
